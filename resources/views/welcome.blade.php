50c0a252857c62d96844c9357076a6c9bf465bd9
jonathan <grandmastermch@hotmail.com>
2019-09-26 16:51:22 -0500

    Titulo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Cerveza Origenes
                 </div>
 
                 <div class="links">
