a22886cc5506b3f1a70093ecd7b1a6085ac15b70
jonathan <grandmastermch@hotmail.com>
2019-11-21 23:12:28 -0600

    Agregado de validaciones en Español, cambio a es

--- resources/views/categories/form.blade.php
+++ resources/views/categories/form.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container section-padding">
   <div class="row justify-content-center">
-    <div class="col-md-10">
+    <div class="col-md-8">
       <div class="card uper">
         <h2 class="title text-center"><span class="deco">Agregar categoria</span></h2>
         <div class="card-body">
@@ -11,7 +11,7 @@
                 <div class="form-group">
                     @csrf
                     <label for="name">Nombre:</label>
-                    <input type="text" class="form-control" name="name"/>
+                    <input type="text" class="form-control" name="name" value="{{ old('name') }}"/>
                 </div>
                 <button type="submit" class="btn btn-primary">Agregar</button>
             </form>
